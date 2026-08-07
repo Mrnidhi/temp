f9e295f20c51f9b50fbf8e131db5aa198f387005
Srinidhi Gowda <srinidhi.connects@gmail.com>
2026-08-07 01:21:39 -0700

    feat: add SQL extraction scripts for LTD cancellations and reschedules

---
+++ ltd_cancellations_extract.sql
@@ -0,0 +1,6 @@
+select
+    ORDER_ID,
+    TTP_DATE,
+    SNAPSHOT_DATE_TIME_CURR,
+    CANCELLATION_REASON
+from LTD_Cancellations;
---
+++ ltd_reschedules_extract.sql
@@ -0,0 +1,6 @@
+select
+    ORDER_ID,
+    TTP_DATE_PREV,
+    SNAPSHOT_DATE_TIME_CURR,
+    RESCHEDULED_CATEGORY
+from LTD_Reschedules;
